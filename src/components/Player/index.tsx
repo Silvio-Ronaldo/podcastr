0523c5b64bb835be57b6598901fa43a6ab9d4a1f
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-04-24 19:33:42 -0300

    Desabilitando botoes previous/next em casos inexistentes

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -18,7 +18,9 @@ export function Player () {
         togglePlay, 
         setPlayingState,
         playNext,
-        playPrevious
+        playPrevious,
+        hasPrevious,
+        hasNext
     } = useContext(PlayerContext);
 
     const episode = episodeList[currentEpisodeIndex];
@@ -91,7 +93,7 @@ export function Player () {
                     <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar" />
                     </button>
-                    <button type="button" onClick={playPrevious} disabled={!episode}>
+                    <button type="button" onClick={playPrevious} disabled={!episode || !hasPrevious}>
                         <img src="/play-previous.svg" alt="Tocar anterior" />
                     </button>
                     <button 
@@ -106,7 +108,7 @@ export function Player () {
                             <img src="/play.svg" alt="Tocar" />
                         ) }
                     </button>
-                    <button type="button" onClick={playNext} disabled={!episode}>
+                    <button type="button" onClick={playNext} disabled={!episode || !hasNext}>
                         <img src="/play-next.svg" alt="Tocar próximo" />
                     </button>
                     <button type="button" disabled={!episode}>
--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -18,6 +18,8 @@ type PlayerContextData = {
     playNext: () => void;
     playPrevious: () => void;
     setPlayingState: (state: boolean) => void;
+    hasPrevious: boolean;
+    hasNext: boolean;
 }
 
 export const PlayerContext = createContext({} as PlayerContextData);
@@ -51,16 +53,17 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
         setIsPlaying(state);
     }
 
-    function playNext() {
-        const nextEpisodeIndex = currentEpisodeIndex + 1;
+    const hasPrevious = currentEpisodeIndex > 0;
+    const hasNext = (currentEpisodeIndex + 1) < episodeList.length;
 
-        if (nextEpisodeIndex < episodeList.length) {
-            setCurrentEpisodeIndex(nextEpisodeIndex);
+    function playNext() {
+        if (hasNext) {
+            setCurrentEpisodeIndex(currentEpisodeIndex + 1);
         }
     }
 
     function playPrevious() {
-        if (currentEpisodeIndex > 0) {
+        if (hasPrevious) {
             setCurrentEpisodeIndex(currentEpisodeIndex - 1);
         }
     }
@@ -76,7 +79,9 @@ export function PlayerContextProvider({ children }: PlayerContextProviderProps) 
                 playPrevious,
                 playList,
                 togglePlay, 
-                setPlayingState 
+                setPlayingState,
+                hasPrevious,
+                hasNext
             }}
         >
             {children}
