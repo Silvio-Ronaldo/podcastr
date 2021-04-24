4c41d6466177554218eefb52c2d542254ed657c4
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-04-24 04:30:10 -0300

    Estilizando botoes do player

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -41,25 +41,33 @@ export function Player () {
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-
+                        { episode ? (
+                            <Slider 
+                                trackStyle={{ backgroundColor: '#04d361' }}
+                                railStyle={{ backgroundColor: '#9f75ff' }}
+                                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+                            />
+                        ) : (
+                            <div className={styles.emptySlider} />    
+                        ) }
                     </div>
                     <span>00:00</span>
                 </div>
 
                 <div className={styles.buttons}>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/shuffle.svg" alt="Embaralhar" />
                     </button>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/play-previous.svg" alt="Tocar anterior" />
                     </button>
-                    <button type="button" className={styles.playButton}>
+                    <button type="button" className={styles.playButton} disabled={!episode}>
                         <img src="/play.svg" alt="Tocar" />
                     </button>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/play-next.svg" alt="Tocar próximo" />
                     </button>
-                    <button type="button">
+                    <button type="button" disabled={!episode}>
                         <img src="/repeat.svg" alt="Repetir" />
                     </button>
                 </div>
