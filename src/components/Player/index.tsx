249bca7f42b1b12a4e0e0786cf607867dfa1bf9d
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-04-24 04:16:56 -0300

    Compartilhando dados pelo contexto

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,18 @@
+import { useContext } from 'react';
+import Image from 'next/image';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+
+import { PlayerContext } from '../../contexts/PlayerContext';
+
 import styles from './styles.module.scss';
 
 export function Player () {
+    const { episodeList, currentEpisodeIndex } = useContext(PlayerContext);
+
+    const episode = episodeList[currentEpisodeIndex];
+
     return (
         <div className={styles.playerContainer}>
             <header>
@@ -8,15 +20,28 @@ export function Player () {
                 <strong>Tocando agora</strong>
             </header>
 
-            <div className={styles.emptyPlayer}>
-                <strong>Selecione um podcast para ouvir</strong>
-            </div>
+            { episode ? (
+                <div className={styles.currentEpisode}>
+                    <Image
+                        width={192}
+                        height={192}
+                        src={episode.thumbnail}
+                        objectFit="cover"
+                    />
+                    <strong>{episode.title}</strong>
+                    <span>{episode.members}</span>
+                </div>
+            ) : (
+                <div className={styles.emptyPlayer}>
+                    <strong>Selecione um podcast para ouvir</strong>
+                </div>
+            ) }
 
-            <footer className={styles.empty}>
+            <footer className={!episode ? styles.empty : ''}>
                 <div className={styles.progress}>
                     <span>00:00</span>
                     <div className={styles.slider}>
-                        <div className={styles.emptySlider} />
+
                     </div>
                     <span>00:00</span>
                 </div>
