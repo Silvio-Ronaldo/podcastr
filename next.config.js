50da6b4c33aa14ca75989e67543f43e141ad9af7
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-05-18 04:17:10 -0300

    Ativando confirmacao de usuario para baixar atualizacoes

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ module.exports = withPWA({
     future: {webpack5: true},
     pwa: {
         dest: 'public',
+        skipWaiting: false,
     },
     images: {
         domains: ['storage.googleapis.com'],
