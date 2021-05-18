6f34906d7b9630b412549731790c1ad134e7f64c
Silvio Ronaldo <silvio.ronaldo77@gmail.com>
2021-05-18 19:07:32 -0300

    Configurando estrategias de cache para audio e imagens

--- next.config.js
+++ next.config.js
@@ -5,6 +5,30 @@ module.exports = withPWA({
     pwa: {
         dest: 'public',
         skipWaiting: false,
+        runtimeCaching: [
+            {
+                urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+                handler: 'CacheFirst',
+                options: {
+                  cacheName: 'static-image-assets',
+                  expiration: {
+                    maxEntries: 128,
+                    maxAgeSeconds: 24 * 60 * 60 * 30 // 30 days
+                  }
+                }
+            },
+            {
+                urlPattern: /\.(?:mp3|mp4|m4a)$/i,
+                handler: 'CacheFirst',
+                options: {
+                  cacheName: 'static-media-assets',
+                  expiration: {
+                    maxEntries: 32,
+                    maxAgeSeconds: 24 * 60 * 60 * 30 // 30 days
+                  }
+                }
+            },
+        ]
     },
     images: {
         domains: ['storage.googleapis.com'],
